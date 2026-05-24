bb035fe7b0c512276290fa5f5db15014c522af37
Jeremy Blankenship <jerbla88@gmail.com>
2026-05-24 05:55:03 -0500

    style: format failing test file

--- tests/test_export_promising_signals.py
+++ tests/test_export_promising_signals.py
@@ -154,9 +154,7 @@ def test_rank_signal_bucket_priority_is_deterministic() -> None:
 
 def test_has_external_source_url_ignores_malformed_urls() -> None:
     assert (
-        exporter._has_external_source_url(
-            [{"source_id": "src-1", "url": "http://["}]
-        )
+        exporter._has_external_source_url([{"source_id": "src-1", "url": "http://["}])
         is False
     )
 
