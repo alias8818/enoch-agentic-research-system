f8ccfede47a3b81ba19abca1765fe3cadb316c47
Jeremy Blankenship <jerbla88@gmail.com>
2026-06-09 16:16:51 -0500

    test: update SonarQube action pin assertion

--- tests/test_sonar_configuration.py
+++ tests/test_sonar_configuration.py
@@ -92,7 +92,7 @@ def test_sonar_workflow_isolates_coverage_from_secret_bearing_scan_and_uses_node
     assert workflow.index("actions/download-artifact") < sonar_index
     assert "actions/checkout@df4cb1c069e1874edd31b4311f1884172cec0e10" in workflow
     assert (
-        "SonarSource/sonarqube-scan-action@7006c4492b2e0ee0f816d36501671557c97f5995"
+        "SonarSource/sonarqube-scan-action@713881670b6b3676cda39549040e2d88c70d582e"
         in workflow
     )
 
